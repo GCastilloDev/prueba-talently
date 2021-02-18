101e4b3d68bc8c9e72f9f50326d322d8b21a2f6e
tavoGeek <gustavoc.mx@gmail.com>
2021-02-17 18:30:23 -0600

    :computer: ENCABEZADO

---
+++ components/PlayVideo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Play video</h1>
+        <video src="https://vimeo.com/39619054"></video>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
